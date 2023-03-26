c6cef00b1bad110a76e7a042e284488ad5eff7d0
Rohit Singh Majila <115721313+majilaRohit@users.noreply.github.com>
2023-03-26 16:46:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, render_template
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier 
 from sklearn import metrics
+from flask import Flask, request, render_template
+import re
+import math
 import pickle
 
 app = Flask("__name__")
@@ -10,9 +12,9 @@ app = Flask("__name__")
 q = ""
 
 
-# @app.route("/")
-# def loadPage():
-#    return render_template('home.html', query='')
+@app.route("/")
+def loadPage():
+    return render_template('home.html', query='')
 
 
 @app.route("/predict", methods=['POST'])
@@ -43,7 +45,7 @@ def cancerPrediction():
         o2 = "Confidence: {}".format(probability*100)
     if single==0:
         o1 = "The patient is not diagnosed with breast Cancer"
-        o2 = "Confidence: {}".format(probability*100)
+        o2 = ""
 
     return render_template('home.html', output1=o1, output2=o2, query1= request.form['query1'], query2= request.form['query2'], query3= request.form['query3'], query4= request.form['query4'], query5= request.form['query5'])
 
